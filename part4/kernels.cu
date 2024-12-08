41b43cf4e3ac65e241676e6cf2ae670c140294b7
pol <polescola3000@gmail.com>
2024-12-08 19:35:31 +0100

    first testing

--- part4/kernels.cu
+++ part4/kernels.cu
@@ -7,23 +7,48 @@ __global__ void gpu_Heat (float *h, float *g, int N) {
 
 	// TODO: kernel computation
 	//...
+
+	extern  __shared__ double sh_mem[];
 	
 	int j = threadIdx.x + blockDim.x * blockIdx.x;
 	int i = threadIdx.y + blockDim.y * blockIdx.y;
-	/*if(i==0 &&  j==0){
+
+	int s_idx = threadIdx.y * (blockDim.x + 2) + threadIdx.x;
+
+
+	
+    if ( threadIdx.x == 0 && j > 0) { // Left halo
+        sh_mem[s_idx - 1] = h[ i*N + (j-1)];
+    }	
+	printf("kernel\n");
+    if ( threadIdx.x == blockDim.x - 1 && j < N - 1) { // Right halo
+        sh_mem[s_idx + 1] = h[i*N  + (j+1)];
+    }
+    if ( threadIdx.y == 0 && i > 0) { // Top halo
+        sh_mem[s_idx - (blockDim.x + 2)] = h[(i-1)*N + j ];
+    }
+    if ( threadIdx.y == blockDim.y - 1 && i < N - 1) { // Bottom halo
+        sh_mem[s_idx + (blockDim.x + 2)] = h[(i+1)*N + j ];
+    }
+	__syncthreads();
+
+	if(i==0 &&  j==0){
 		printf("kernel\n");
 		    for (int k = 0; k <N*N; k++) {    
-        printf("%f ",h[k]);
+        printf("%f ",sh_mem[k]);
     }
-	}*/
+	}
+	
+	
+
 
 	if(i<(N-1) && j<(N-1) && i > 0 && j>0){	
 		
 		
-		g[i*N+j]= 0.25 * (h[ i*N     + (j-1) ]+  // left
-				h[ i*N     + (j+1) ]+  // right
-					h[ (i-1)*N + j     ]+  // top
-					h[ (i+1)*N + j     ]); // bottom
+		g[i*N+j]= 0.25 * (sh_mem[s_idx - 1] +  // Left
+                           sh_mem[s_idx + 1] +  // Right
+                           sh_mem[s_idx - (blockDim.x + 2)] +  // Top
+                           sh_mem[s_idx + (blockDim.x + 2)]); // Bottom
 		
 	
 	}
